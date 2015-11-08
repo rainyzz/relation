eab20b3a60f48a1b7cc53632c0191594d577cc53
rainystars <zbestz@gmail.com>
2015-11-08 18:06:45 +0800

    添加标准文献数据的处理部分

--- src/main/java/com/rainyzz/relation/util/LineReader.java
+++ src/main/java/com/rainyzz/relation/util/LineReader.java
@@ -1,7 +1,9 @@
 package com.rainyzz.relation.util;
 
+import com.alibaba.fastjson.JSON;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Maps;
+import org.codehaus.jettison.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.List;
@@ -32,6 +34,14 @@ public class LineReader {
         return article;
 
     }
+
+    public static Map<String,String> readRecord(String line){
+        Map<String,String> article = JSON.parseObject(line,Map.class);
+        System.out.println(article);
+        return  article;
+    }
+
+
     public static String[] readRecordToList(String line,int start,int fromEnd){
 
         String content = line.substring(start, line.length() - fromEnd);
@@ -40,7 +50,7 @@ public class LineReader {
 
     }
     public static void main(String args[]){
-        String s = "INSERT INTO `wanfang_detail` VALUES ('1', '有害元素对高炉内焦炭热态性能的影响', 'Effect of Harmful Elements on Thermal Properties of Cokes in BF', '针对有害元素在高炉内的反应行为和对焦炭劣化的催化作用,分析了钾、钠、锌、铅、氯化物在高炉内的循环富集过程,重点探讨了其对焦炭热态性能的影响,进一步明确了几种有害元素对焦炭和高炉冶炼的危害性,并根据其循环富集特点提出了控制措施.', '', '张伟,王再义,张立国,王亮,韩子文', ' Zhang Wei , Wang Zaiyi , Zhang Liguo , Wang Liang , Han Ziwen ', '鞍钢股份有限公司技术中心', '鞍钢技术', 'Angang Technology', '2013,(5)', '高炉,有害元素,焦炭', '', 'http://d.wanfangdata.com.cn/Periodical_agjs201305002.aspx', '41.152991112691,122.93622425434');\n";
+       /* String s = "INSERT INTO `wanfang_detail` VALUES ('1', '有害元素对高炉内焦炭热态性能的影响', 'Effect of Harmful Elements on Thermal Properties of Cokes in BF', '针对有害元素在高炉内的反应行为和对焦炭劣化的催化作用,分析了钾、钠、锌、铅、氯化物在高炉内的循环富集过程,重点探讨了其对焦炭热态性能的影响,进一步明确了几种有害元素对焦炭和高炉冶炼的危害性,并根据其循环富集特点提出了控制措施.', '', '张伟,王再义,张立国,王亮,韩子文', ' Zhang Wei , Wang Zaiyi , Zhang Liguo , Wang Liang , Han Ziwen ', '鞍钢股份有限公司技术中心', '鞍钢技术', 'Angang Technology', '2013,(5)', '高炉,有害元素,焦炭', '', 'http://d.wanfangdata.com.cn/Periodical_agjs201305002.aspx', '41.152991112691,122.93622425434');\n";
         String s2 = "有害 元素 高炉 焦炭 性能 影响', '针对 有害 元素 高炉 反应 行为 焦炭 催化 作用 分析 氯化物 高炉 循环 富集 过程 重点 探讨 焦炭 性能 影响 进一步 明确 几种 有害 元素 焦炭 高炉 冶炼 危害性 根据 循环 富集 特点 提出 控制 措施', '张伟,王再义,张立国,王亮,韩子文', '鞍钢股份有限公司技术中心', '鞍钢技术', '2013', '高炉 有害元素 焦炭', '41.152991112691,122.9362242543";
         System.out.println(readRecord(s2,0,0));
         String ss = "信息 动态', '', '', '', '鞍钢技术', '2013', ''  NU";
@@ -48,6 +58,7 @@ public class LineReader {
         System.out.println(data.length);
         for(int i =0; i < data.length; i++){
             System.out.println(i +":" + data[i]);
-        }
+        }*/
+        readRecord("{\"A001\":\"8021418\",\"A100\":\"GB 51139-2015\",\"A104\":\"CN-GB\",\"A885\":\"CN\",\"_version_\":1516988881709301760,\"id\":\"8021418\",\"title_c\":\"纤维素 纤维 工厂 设计 规范\"}");
     }
 }
--- src/main/java/com/rainyzz/relation/util/TransFormer.java
+++ src/main/java/com/rainyzz/relation/util/TransFormer.java
@@ -1,6 +1,21 @@
 package com.rainyzz.relation.util;
 
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import org.ansj.domain.Term;
+import org.ansj.splitWord.analysis.ToAnalysis;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+
+
 import java.io.*;
+import java.util.List;
 
 /**
  * Created by rainystars on 10/26/2015.
@@ -8,7 +23,81 @@ import java.io.*;
 public class TransFormer {
     public static final String SQL_FILE_PATH = "C:\\Users\\rainystars\\Desktop\\wanfang_detail.sql";
     public static final String SQL_FILE_OUTPUT_PATH = "D://wanfang.txt";
-    public static void main(String[] args){
+    public static final String OUTPUT_PATH = "D://standard.txt";
+
+    public static void readFromSolr(){
+        SolrClient solr = new HttpSolrClient("http://localhost:8983/solr/dev");
+        SolrQuery query = new SolrQuery("A104:CN-GB");
+        query.setRows(60000);
+
+        QueryResponse response = null;
+        try {
+            response = solr.query(query);
+        } catch (SolrServerException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(response == null){
+            return;
+        }
+        SolrDocumentList list = response.getResults();
+        File writename = new File(OUTPUT_PATH);
+        BufferedWriter out = null;
+
+        try {
+            out = new BufferedWriter(new FileWriter(writename));
+
+            for(SolrDocument doc:list){
+
+                JSONObject jb = new JSONObject(doc);
+                if(jb.containsKey("title_c")){
+                    jb.put("title_c",token(jb.get("title_c").toString()));
+                }
+                if(jb.containsKey("des_c")){
+                    jb.put("des_c",token(jb.get("des_c").toString()));
+                }
+                if(jb.containsKey("keyword_c")){
+                    jb.put("keyword_c",token(jb.get("keyword_c").toString()));
+                }
+                out.write(jb + "\n");
+                System.out.println(jb);
+                //break;
+            }
+            out.flush();
+            out.close();
+        }catch (IOException e){
+
+        }
+
+
+    }
+
+    private static String token(String s){
+
+        List<Term> terms = ToAnalysis.parse(s);
+        List<String> words = Lists.newArrayList();
+
+        for(Term term:terms) {
+            String word = term.getName();
+            if(word.length() < 2){
+                continue;
+            }
+            char w = word.charAt(0);
+            if(('a' <= w && w <= 'z') || ('A' <= w && w <= 'Z')  || Character.isDigit(w)){
+                continue;
+            }
+            if(w == '#' || w == '\'' || w == '%' || w== '@' || w=='&' || w =='—'){
+                continue;
+            }
+            words.add(term.getName());
+        }
+
+        String tokens = Joiner.on(" ").skipNulls().join(words);
+        return tokens;
+    }
+
+    public void transform(){
         File file = new File(SQL_FILE_PATH);
         BufferedReader reader = null;
         File writename = new File(SQL_FILE_OUTPUT_PATH);
@@ -42,5 +131,10 @@ public class TransFormer {
                 }
             }
         }
+    }
+
+
+    public static void main(String[] args){
+        readFromSolr();
     }
 }
